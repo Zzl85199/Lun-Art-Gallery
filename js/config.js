983c1afadf1e052b6d0fe5fa96d46c363f11f160
Zzl85199 <jonathan40507@gmail.com>
2026-08-03 21:20:10 +0800

    app script change

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@
  * ===============================================================
  */
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzltQ7fTSLQUUQQvR8JkORYGnZ3DwwEps9sj7rowapY2uk14-weQOo8Rj3b4NLqfHkC/exec", // 例如 https://script.google.com/macros/s/xxxxxxxx/exec
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyy3K5xIytIdrQ4RBborQ84HJwcKgT6DOnDmodetreecsW_gW3uDPIS-uRlpfGLAPoQ/exec", // 例如 https://script.google.com/macros/s/xxxxxxxx/exec
   // 在 Google Cloud Console →「憑證」建立的 OAuth 用戶端 ID（應用程式類型：網頁應用程式）
   GOOGLE_CLIENT_ID: "539277836651-l7se69fsu9d6l75r55kpbj2lmr7enhoh.apps.googleusercontent.com",
   SITE_TITLE: "AI 創作畫廊",
